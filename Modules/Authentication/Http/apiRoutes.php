a72c3fba5a2b3e0d7b5da9a71ad9f96209f3ebbc
sonvq-qsoftvn <vuquangson1610@gmail.com>
2018-01-21 18:35:32 +0700

    commit toggle push notification

--- Modules/Authentication/Http/apiRoutes.php
+++ Modules/Authentication/Http/apiRoutes.php
@@ -13,10 +13,12 @@ $api->version('v1', function ($api) {
         
         $api->group(['middleware' => ['apis.frontend']], function () use ($api) {                        
             $api->get('/logout', ['uses' => 'AuthenticationController@logout', 'as' => 'api.authentication.logout']);                        
-            $api->post('/update-profile', ['uses' => 'AuthenticationController@updateProfile', 'as' => 'api.authentication.update.profile']);
+            $api->post('/update-profile', ['uses' => 'AuthenticationController@updateProfile', 'as' => 'api.authentication.update.profile']);            
             $api->get('/get-profile-customer/{id}', ['uses' => 'AuthenticationController@getProfileCustomer', 'as' => 'api.authentication.get.profile.customer']);
             $api->get('/get-profile-washer/{id}', ['uses' => 'AuthenticationController@getProfileWasher', 'as' => 'api.authentication.get.profile.washer']);
             $api->post('/change-password', ['uses' => 'AuthenticationController@changePassword', 'as' => 'api.authentication.change.password']);
+            
+            $api->post('/toggle-push-notification', ['uses' => 'AuthenticationController@togglePushNotification', 'as' => 'api.authentication.toggle.push.notification']);
         });
                 
         
